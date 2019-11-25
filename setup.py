9b1ae4bae2e8a88f1647dd0661d93b1177c32ae5
Jeremy Lainé <jeremy.laine@m4x.org>
2019-11-25 12:49:11 +0100

    [qa] reformat code using black

--- setup.py
+++ setup.py
@@ -4,42 +4,42 @@ import sys
 import setuptools
 
 root_dir = os.path.abspath(os.path.dirname(__file__))
-readme_file = os.path.join(root_dir, 'README.rst')
-with open(readme_file, encoding='utf-8') as f:
+readme_file = os.path.join(root_dir, "README.rst")
+with open(readme_file, encoding="utf-8") as f:
     long_description = f.read()
 
-if os.environ.get('READTHEDOCS') == 'True':
-    cffi_modules=[]
+if os.environ.get("READTHEDOCS") == "True":
+    cffi_modules = []
 else:
-    cffi_modules=['src/_cffi_src/build_srtp.py:ffibuilder']
+    cffi_modules = ["src/_cffi_src/build_srtp.py:ffibuilder"]
 
 setuptools.setup(
-    name='pylibsrtp',
-    version='0.6.3',
-    description='Python wrapper around the libsrtp library',
+    name="pylibsrtp",
+    version="0.6.3",
+    description="Python wrapper around the libsrtp library",
     long_description=long_description,
-    url='https://github.com/aiortc/pylibsrtp',
-    author='Jeremy Lainé',
-    author_email='jeremy.laine@m4x.org',
-    license='BSD',
+    url="https://github.com/aiortc/pylibsrtp",
+    author="Jeremy Lainé",
+    author_email="jeremy.laine@m4x.org",
+    license="BSD",
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Topic :: Communications :: Telephony',
-        'Topic :: Security :: Cryptography',
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Communications :: Telephony",
+        "Topic :: Security :: Cryptography",
     ],
     cffi_modules=cffi_modules,
     package_dir={"": "src"},
     package_data={"pylibsrtp": ["py.typed"]},
-    packages=['pylibsrtp'],
-    install_requires=['cffi>=1.0.0'],
-    setup_requires=['cffi>=1.0.0'],
+    packages=["pylibsrtp"],
+    install_requires=["cffi>=1.0.0"],
+    setup_requires=["cffi>=1.0.0"],
 )
--- tests/test_session.py
+++ tests/test_session.py
@@ -3,21 +3,21 @@ from unittest import TestCase
 from pylibsrtp import Error, Policy, Session
 
 RTP = (
-    b'\x80\x08\x00\x00'  # version, packet type, sequence number
-    b'\x00\x00\x00\x00'  # timestamp
-    b'\x00\x00\x30\x39'  # ssrc: 12345
-) + (b'\xd4' * 160)
+    b"\x80\x08\x00\x00"  # version, packet type, sequence number
+    b"\x00\x00\x00\x00"  # timestamp
+    b"\x00\x00\x30\x39"  # ssrc: 12345
+) + (b"\xd4" * 160)
 RTCP = (
-    b'\x80\xc8\x00\x06\xf3\xcb\x20\x01\x83\xab\x03\xa1\xeb\x02\x0b\x3a'
-    b'\x00\x00\x94\x20\x00\x00\x00\x9e\x00\x00\x9b\x88'
+    b"\x80\xc8\x00\x06\xf3\xcb\x20\x01\x83\xab\x03\xa1\xeb\x02\x0b\x3a"
+    b"\x00\x00\x94\x20\x00\x00\x00\x9e\x00\x00\x9b\x88"
 )
 
 # Set key to predetermined value
 KEY = (
-    b'\x00\x01\x02\x03\x04\x05\x06\x07'
-    b'\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f'
-    b'\x10\x11\x12\x13\x14\x15\x16\x17'
-    b'\x18\x19\x1a\x1b\x1c\x1d'
+    b"\x00\x01\x02\x03\x04\x05\x06\x07"
+    b"\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f"
+    b"\x10\x11\x12\x13\x14\x15\x16\x17"
+    b"\x18\x19\x1a\x1b\x1c\x1d"
 )
 
 
@@ -51,7 +51,7 @@ class PolicyTest(TestCase):
         with self.assertRaises(TypeError) as cm:
             policy.key = 1234
         self.assertEqual(policy.key, None)
-        self.assertEqual(str(cm.exception), 'key must be bytes')
+        self.assertEqual(str(cm.exception), "key must be bytes")
 
     def test_ssrc_type(self):
         policy = Policy()
@@ -81,23 +81,21 @@ class SessionTest(TestCase):
 
         with self.assertRaises(Error) as cm:
             Session(policy=policy)
-        self.assertEqual(str(cm.exception), 'unsupported parameter')
+        self.assertEqual(str(cm.exception), "unsupported parameter")
 
     def test_add_remove_stream(self):
         # protect RTP
-        tx_session = Session(policy=Policy(
-            key=KEY,
-            ssrc_type=Policy.SSRC_SPECIFIC,
-            ssrc_value=12345))
+        tx_session = Session(
+            policy=Policy(key=KEY, ssrc_type=Policy.SSRC_SPECIFIC, ssrc_value=12345)
+        )
         protected = tx_session.protect(RTP)
         self.assertEqual(len(protected), 182)
 
         # add stream and unprotect RTP
         rx_session = Session()
-        rx_session.add_stream(Policy(
-            key=KEY,
-            ssrc_type=Policy.SSRC_SPECIFIC,
-            ssrc_value=12345))
+        rx_session.add_stream(
+            Policy(key=KEY, ssrc_type=Policy.SSRC_SPECIFIC, ssrc_value=12345)
+        )
         unprotected = rx_session.unprotect(protected)
         self.assertEqual(len(unprotected), 172)
         self.assertEqual(unprotected, RTP)
@@ -108,74 +106,64 @@ class SessionTest(TestCase):
         # try removing stream again
         with self.assertRaises(Error) as cm:
             rx_session.remove_stream(12345)
-        self.assertEqual(str(cm.exception), 'no appropriate context found')
+        self.assertEqual(str(cm.exception), "no appropriate context found")
 
     def test_rtp_any_ssrc(self):
         # protect RTP
-        tx_session = Session(policy=Policy(
-            key=KEY,
-            ssrc_type=Policy.SSRC_ANY_OUTBOUND))
+        tx_session = Session(policy=Policy(key=KEY, ssrc_type=Policy.SSRC_ANY_OUTBOUND))
         protected = tx_session.protect(RTP)
         self.assertEqual(len(protected), 182)
 
         # bad type
         with self.assertRaises(TypeError) as cm:
             tx_session.protect(4567)
-        self.assertEqual(str(cm.exception), 'packet must be bytes')
+        self.assertEqual(str(cm.exception), "packet must be bytes")
 
         # bad length
         with self.assertRaises(ValueError) as cm:
-            tx_session.protect(b'0' * 1500)
-        self.assertEqual(str(cm.exception), 'packet is too long')
+            tx_session.protect(b"0" * 1500)
+        self.assertEqual(str(cm.exception), "packet is too long")
 
         # unprotect RTP
-        rx_session = Session(policy=Policy(
-            key=KEY,
-            ssrc_type=Policy.SSRC_ANY_INBOUND))
+        rx_session = Session(policy=Policy(key=KEY, ssrc_type=Policy.SSRC_ANY_INBOUND))
         unprotected = rx_session.unprotect(protected)
         self.assertEqual(len(unprotected), 172)
         self.assertEqual(unprotected, RTP)
 
     def test_rtcp_any_ssrc(self):
         # protect RCTP
-        tx_session = Session(policy=Policy(
-            key=KEY,
-            ssrc_type=Policy.SSRC_ANY_OUTBOUND))
+        tx_session = Session(policy=Policy(key=KEY, ssrc_type=Policy.SSRC_ANY_OUTBOUND))
         protected = tx_session.protect_rtcp(RTCP)
         self.assertEqual(len(protected), 42)
 
         # bad type
         with self.assertRaises(TypeError) as cm:
             tx_session.protect_rtcp(4567)
-        self.assertEqual(str(cm.exception), 'packet must be bytes')
+        self.assertEqual(str(cm.exception), "packet must be bytes")
 
         # bad length
         with self.assertRaises(ValueError) as cm:
-            tx_session.protect_rtcp(b'0' * 1500)
-        self.assertEqual(str(cm.exception), 'packet is too long')
+            tx_session.protect_rtcp(b"0" * 1500)
+        self.assertEqual(str(cm.exception), "packet is too long")
 
         # unprotect RTCP
-        rx_session = Session(policy=Policy(
-            key=KEY,
-            ssrc_type=Policy.SSRC_ANY_INBOUND))
+        rx_session = Session(policy=Policy(key=KEY, ssrc_type=Policy.SSRC_ANY_INBOUND))
         unprotected = rx_session.unprotect_rtcp(protected)
         self.assertEqual(len(unprotected), 28)
         self.assertEqual(unprotected, RTCP)
 
     def test_rtp_specific_ssrc(self):
         # protect RTP
-        tx_session = Session(policy=Policy(
-            key=KEY,
-            ssrc_type=Policy.SSRC_SPECIFIC,
-            ssrc_value=12345))
+        tx_session = Session(
+            policy=Policy(key=KEY, ssrc_type=Policy.SSRC_SPECIFIC, ssrc_value=12345)
+        )
         protected = tx_session.protect(RTP)
         self.assertEqual(len(protected), 182)
 
         # unprotect RTP
-        rx_session = Session(policy=Policy(
-            key=KEY,
-            ssrc_type=Policy.SSRC_SPECIFIC,
-            ssrc_value=12345))
+        rx_session = Session(
+            policy=Policy(key=KEY, ssrc_type=Policy.SSRC_SPECIFIC, ssrc_value=12345)
+        )
         unprotected = rx_session.unprotect(protected)
         self.assertEqual(len(unprotected), 172)
         self.assertEqual(unprotected, RTP)
