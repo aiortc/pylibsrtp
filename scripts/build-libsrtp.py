9f30401f98594b3648e18b9abedbda129f94ec1c
Jeremy Lainé <jeremy.laine@m4x.org>
2022-02-01 17:22:18 +0100

    [build] switch build script to Python, set CMAKE_OSX_ARCHITECTURES
    Fixes: #21

---
+++ scripts/build-libsrtp.py
@@ -0,0 +1,39 @@
+import os
+import platform
+import shutil
+import subprocess
+import sys
+
+if len(sys.argv) < 2:
+    sys.stderr.write("Usage: build-libsrtp.py <prefix>\n")
+    sys.exit(1)
+
+dest_dir = sys.argv[1]
+build_dir = os.path.abspath("build")
+
+for d in [build_dir, dest_dir]:
+    if os.path.exists(d):
+        shutil.rmtree(d)
+
+
+def run(cmd):
+    sys.stdout.write(f"- Running: {cmd}\n")
+    subprocess.run(cmd, check=True, stderr=sys.stderr.buffer, stdout=sys.stdout.buffer)
+
+
+cmake_args = [
+    "-DCMAKE_INSTALL_PREFIX=" + dest_dir,
+    "-DCMAKE_POSITION_INDEPENDENT_CODE=ON",
+    "-DCMAKE_VERBOSE_MAKEFILE:BOOL=ON",
+]
+if platform.system() == "Darwin" and "ARCHFLAGS" in os.environ:
+    archs = [x for x in os.environ["ARCHFLAGS"].split() if x != "-arch"]
+    cmake_args.append("-DCMAKE_OSX_ARCHITECTURES=" + ";".join(archs))
+
+run(["git", "clone", "https://github.com/cisco/libsrtp/", build_dir])
+os.chdir(build_dir)
+run(["git", "checkout", "-qf", "v2.4.2"])
+
+run(["cmake", "."] + cmake_args)
+run(["make"])
+run(["make", "install"])
