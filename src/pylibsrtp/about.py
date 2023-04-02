a7c954d9b42c0d542c2d02117770d5a6cbb7e9e4
Jeremy Lainé <jeremy.laine@m4x.org>
2023-04-02 23:29:51 +0200

    Move package metadata to pyproject.toml

--- src/pylibsrtp/about.py
+++
@@ -1,7 +0,0 @@
-__author__ = "Jeremy Lainé"
-__email__ = "jeremy.laine@m4x.org"
-__license__ = "BSD"
-__summary__ = "Python wrapper around the libsrtp library"
-__title__ = "pylibsrtp"
-__uri__ = "https://github.com/aiortc/pylibsrtp"
-__version__ = "0.8.0"
