cf723b72e13b6a270b08c1357a8248a875c099c1
Jeremy Lainé <jeremy.laine@m4x.org>
2023-11-30 19:27:03 +0100

    Build wheels using the Python limited ABI

--- setup.py
+++ setup.py
@@ -1,10 +1,25 @@
 import os
 
 import setuptools
+from wheel.bdist_wheel import bdist_wheel
+
+
+class bdist_wheel_abi3(bdist_wheel):
+    def get_tag(self):
+        python, abi, plat = super().get_tag()
+
+        if python.startswith("cp"):
+            return "cp38", "abi3", plat
+
+        return python, abi, plat
+
 
 if os.environ.get("READTHEDOCS") == "True":
     cffi_modules = []
 else:
     cffi_modules = ["src/_cffi_src/build_srtp.py:ffibuilder"]
 
-setuptools.setup(cffi_modules=cffi_modules)
+setuptools.setup(
+    cffi_modules=cffi_modules,
+    cmdclass={"bdist_wheel": bdist_wheel_abi3},
+)
