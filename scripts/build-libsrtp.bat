f4d2315bf736b7eac1d9dbee5cf58123301a2728
Jeremy Lainé <jeremy.laine@m4x.org>
2025-02-02 15:16:03 +0100

    Build wheels against libsrtp v2.6.0
    The build now needs to be performed outside of the source directory.

--- scripts/build-libsrtp.bat
+++ scripts/build-libsrtp.bat
@@ -1,6 +1,8 @@
-set destdir=%1
+set dest_dir=%1
+set build_dir=libsrtp.build
+set source_dir=libsrtp.source
 
-python scripts\fetch-vendor.py %destdir%
+python scripts\fetch-vendor.py %dest_dir%
 
 for %%d in (libsrtp) do (
     if exist %%d (
@@ -8,24 +10,28 @@ for %%d in (libsrtp) do (
     )
 )
 
-git clone https://github.com/cisco/libsrtp/
-cd libsrtp
-git checkout -qf v2.5.0
+git clone https://github.com/cisco/libsrtp/ %source_dir%
+cd %source_dir%
+git checkout -qf v2.6.0
+cd ..
 
 if "%PYTHON_ARCH%" == "64" (
     set CMAKE_OPTIONS=-A x64
 ) else (
     set CMAKE_OPTIONS=-A Win32
 )
-cmake . -G "Visual Studio 17 2022" %CMAKE_OPTIONS% -DENABLE_OPENSSL=ON
+mkdir %build_dir%
+cd %build_dir%
+cmake ..\%source_dir% -G "Visual Studio 17 2022" %CMAKE_OPTIONS% -DENABLE_OPENSSL=ON
 cmake --build . --config Release
+cd ..
 
-mkdir %destdir%
-mkdir %destdir%\include
-mkdir %destdir%\include\srtp2
-mkdir %destdir%\lib
+mkdir %dest_dir%
+mkdir %dest_dir%\include
+mkdir %dest_dir%\include\srtp2
+mkdir %dest_dir%\lib
 
 for %%d in (include\srtp.h crypto\include\auth.h crypto\include\cipher.h crypto\include\crypto_types.h) do (
-	 copy %%d %destdir%\include\srtp2
+	 copy %source_dir%\%%d %dest_dir%\include\srtp2
 )
-copy Release\srtp2.lib %destdir%\lib\srtp2.lib
+copy %build_dir%\Release\srtp2.lib %dest_dir%\lib\srtp2.lib
